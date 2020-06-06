c4901c7b21af66e64dd3fc9ef4182d93d7fe141b
Antoine De Vuyst <fortyonepointsix@gmail.com>
2020-06-06 12:24:22 -0400

    0.7

--- PosterManager.js
+++ PosterManager.js
@@ -6,7 +6,7 @@ async function populateCreatorSelect(){
   console.log("Populate Poster Select")
 
   for (let j = 0; j<ubounties.length;j++){
-    if(users[ubounties[j].creatorIndex]==signer._address && ubounties[j].numLeft!=0){
+    if(users[ubounties[j].creatorIndex]==signer._address && ubounties[j].available!=0){
       var opt = document.createElement("option");
       console.log(j)
      opt.value= j;
